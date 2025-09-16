ad901b3c589fb10bf441441ebc3a84afaebc1d3b
rossiwan <rossiwan123@gmail.com>
2025-09-16 08:27:49 +0700

    clean: remove unused component imports from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,8 @@ import GovernmentApp from './components/gover.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/*<App />*/}
-    {/*<TouchList />*/}
-    {/*<Grade />*/}
+    
     {<GovernmentApp />}
-    {/*<Hook />*/}
+    
   </StrictMode>,
 )
